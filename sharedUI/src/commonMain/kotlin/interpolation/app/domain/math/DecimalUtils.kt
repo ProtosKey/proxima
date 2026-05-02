0b9bdb394df405b61d941ca5aa0de735ca2504f9
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-03 02:54:19 +0300

    feat: added exp, pow functions for math components

--- sharedUI/src/commonMain/kotlin/interpolation/app/domain/math/DecimalUtils.kt
+++ sharedUI/src/commonMain/kotlin/interpolation/app/domain/math/DecimalUtils.kt
@@ -44,9 +44,42 @@ fun BigDecimal.ln(decimalMode: DecimalMode): BigDecimal {
     return result * 2
 }
 
+fun BigDecimal.exp(decimalMode: DecimalMode): BigDecimal {
+    if (this.isZero()) {
+        return BigDecimal.ONE
+    } else if (this.signum() == -1) {
+        return BigDecimal.ONE.divide(this.negate().exp(decimalMode), decimalMode)
+    }
+
+    var rem = BigDecimal.ONE
+    var res = rem
+    for (n in 1 until DecimalUtils.EXP_REPEATS) {
+        rem = (rem * this).divide(n.toBigDecimal(), decimalMode)
+        if (rem.isZero()) {
+            break
+        }
+        res += rem
+    }
+    return res
+}
+
+fun BigDecimal.pow(add: BigDecimal, decimalMode: DecimalMode): BigDecimal {
+    if (add.compareTo(BigDecimal.ONE) == 0) {
+        return this
+    } else if (add.isZero()) {
+        return BigDecimal.ONE
+    } else if (this.isZero()) {
+        return BigDecimal.ZERO
+    } else if (this.compareTo(BigDecimal.ONE) == 0) {
+        return BigDecimal.ONE
+    }
+    return (add * this.ln(decimalMode)).exp(decimalMode)
+}
+
 object DecimalUtils {
     val DIVIDE_MODE =
         DecimalMode(decimalPrecision = 80, roundingMode = RoundingMode.ROUND_HALF_FLOOR)
     const val SQRT_REPEATS = 10
     const val LN_REPEATS = 100
+    const val EXP_REPEATS = 200
 }
