20c7fa53bac95ab4ee6a252ef4510c9a70fa6ca8
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-03 03:08:01 +0300

    feat: added solver for interpolation

---
+++ sharedUI/src/commonMain/kotlin/interpolation/app/domain/solver/ExponentSolver.kt
@@ -0,0 +1,30 @@
+package interpolation.app.domain.solver
+
+import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import interpolation.app.domain.basic.CanSolve
+import interpolation.app.domain.exception.SolverException
+import interpolation.app.domain.math.DecimalUtils
+import interpolation.app.domain.math.InterpolationEngine
+import interpolation.app.domain.math.exp
+import interpolation.app.domain.math.ln
+import interpolation.app.domain.model.Coordinates
+import interpolation.app.domain.model.Function
+
+object ExponentSolver : CanSolve {
+    override fun solve(points: Coordinates): Function {
+        val lat = mutableListOf<BigDecimal>()
+        val lon = mutableListOf<BigDecimal>()
+
+        points.forEach { point ->
+            if (point.y <= 0) {
+                throw SolverException("Аргумент должен быть положительным")
+            }
+
+            lat.add(point.x)
+            lon.add(point.y.ln(DecimalUtils.DIVIDE_MODE))
+        }
+
+        val result = InterpolationEngine.calculate(lat, lon, Function.Exponent.size)
+        return Function.Exponent(result[0].exp(DecimalUtils.DIVIDE_MODE), result[0])
+    }
+}
