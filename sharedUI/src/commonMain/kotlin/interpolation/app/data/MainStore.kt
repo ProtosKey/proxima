56dbca006e1a2acd380e3af5fc1bf503d875a8a0
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-07 01:35:49 +0300

    feat: added data package for global data in app

---
+++ sharedUI/src/commonMain/kotlin/interpolation/app/data/MainStore.kt
@@ -0,0 +1,45 @@
+package interpolation.app.data
+
+import interpolation.app.data.model.FunctionResult
+import interpolation.app.data.model.FunctionType
+import interpolation.app.data.model.MessageType
+import interpolation.app.data.model.Notification
+import interpolation.app.domain.model.Point
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+object MainStore {
+    private val _points = MutableStateFlow<List<Point>>(emptyList())
+    val points = _points.asStateFlow()
+
+    private val _results = MutableStateFlow<Map<FunctionType, FunctionResult>>(emptyMap())
+    val results = _results.asStateFlow()
+
+    private val _notification = MutableStateFlow(Notification())
+    val notification = _notification.asStateFlow()
+
+    fun updatePoints(points: List<Point>) {
+        _points.value = points
+    }
+
+    fun updateFunctions(results: Map<FunctionType, FunctionResult>) {
+        _results.value = results
+    }
+
+    fun showMessage(message: String, messageType: MessageType) {
+        _notification.update {
+            it.copy(
+                message = message, messageType = messageType
+            )
+        }
+    }
+
+    fun hideMessage() {
+        _notification.update {
+            it.copy(
+                isVisible = false
+            )
+        }
+    }
+}
---
+++ sharedUI/src/commonMain/kotlin/interpolation/app/data/utils/Defaults.kt
@@ -0,0 +1,50 @@
+package interpolation.app.data.utils
+
+import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import com.ionspin.kotlin.bignum.decimal.toBigDecimal
+import interpolation.app.data.model.FunctionType
+import interpolation.app.data.model.MessageType
+import interpolation.app.domain.basic.CanSolve
+import interpolation.app.domain.model.Coordinates
+import interpolation.app.domain.model.Point
+import interpolation.app.domain.solver.CubicSolver
+import interpolation.app.domain.solver.ExponentSolver
+import interpolation.app.domain.solver.LinearSolver
+import interpolation.app.domain.solver.LogarithmSolver
+import interpolation.app.domain.solver.PowerSolver
+import interpolation.app.domain.solver.SquareSolver
+import kotlin.random.Random
+
+object Defaults {
+    private const val POINT_COUNT = 3
+
+    fun exception(): String = "Неожиданная ошибка"
+
+    fun message(): String = "Начните работу..."
+
+    fun messageType(): MessageType = MessageType.GOOD
+
+    fun visible(): Boolean = false
+
+    fun coordinates(): Coordinates {
+        val points = MutableList(POINT_COUNT) { randomPoint() }
+        return Coordinates(points)
+    }
+
+    fun solvers(): Map<FunctionType, CanSolve> = mapOf(
+        FunctionType.THIRD to CubicSolver,
+        FunctionType.EXPONENT to ExponentSolver,
+        FunctionType.LINEAR to LinearSolver,
+        FunctionType.LOGARITHM to LogarithmSolver,
+        FunctionType.POWER to PowerSolver,
+        FunctionType.SECOND to SquareSolver
+    )
+
+    private fun randomPoint(): Point {
+        return Point(randomBigDecimal(), randomBigDecimal())
+    }
+
+    private fun randomBigDecimal(): BigDecimal {
+        return ((Random.Default.nextDouble() - .5) * 20).toBigDecimal()
+    }
+}
