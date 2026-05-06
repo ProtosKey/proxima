98eeeefb546e97e4aa49b7bde3f7c0c58ca39d3c
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-06 16:35:46 +0300

    refactor: now parser use mappers

--- sharedUI/src/commonMain/kotlin/interpolation/app/presentation/parser/ModelParser.kt
+++ sharedUI/src/commonMain/kotlin/interpolation/app/presentation/parser/ModelParser.kt
@@ -1,20 +1,17 @@
 package interpolation.app.presentation.parser
 
 import interpolation.app.domain.model.Coordinates
+import interpolation.app.presentation.mapper.EntryMapper
+import interpolation.app.presentation.mapper.LabelMapper
 import interpolation.app.presentation.model.PointEntry
 import interpolation.app.presentation.model.PointLabel
 
 object ModelParser {
     fun parsePointsToNumbers(points: Coordinates): MutableList<PointEntry> {
-        return points.map { point -> PointEntry(point.x, point.y) }.toMutableList()
+        return points.map { point -> EntryMapper.mapTo(point) }.toMutableList()
     }
 
     fun parsePointsToLabel(points: Coordinates): MutableList<PointLabel> {
-        return points.map { point ->
-            PointLabel(
-                StringParser.prepareToString(point.x),
-                StringParser.prepareToString(point.y)
-            )
-        }.toMutableList()
+        return points.map { point -> LabelMapper.mapTo(point) }.toMutableList()
     }
 }
