36bc11d23c5646620a33f5ed904ca12dad9b8df4
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-07 01:46:46 +0300

    feat: added method for show message

--- sharedUI/src/commonMain/kotlin/interpolation/app/presentation/basic/HaveMessage.kt
+++ sharedUI/src/commonMain/kotlin/interpolation/app/presentation/basic/HaveMessage.kt
@@ -3,6 +3,6 @@ package interpolation.app.presentation.basic
 import interpolation.app.data.model.MessageType
 
 interface HaveMessage {
-    fun close()
-    fun open(message: String, messageType: MessageType)
+    fun hideMessage()
+    fun showMessage(message: String, messageType: MessageType)
 }
