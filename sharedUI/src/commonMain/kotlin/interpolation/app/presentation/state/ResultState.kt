68918f7211f7b7732c2148a0f9d78bb100389007
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-07 02:28:49 +0300

    feat: added state for result table

---
+++ sharedUI/src/commonMain/kotlin/interpolation/app/presentation/state/ResultState.kt
@@ -0,0 +1,11 @@
+package interpolation.app.presentation.state
+
+import interpolation.app.data.model.FunctionResult
+import interpolation.app.data.model.FunctionType
+
+data class ResultState(
+    val results: Map<FunctionType, FunctionResult> = emptyMap(),
+    val selected: Map<FunctionType, Boolean> = emptyMap(),
+    val best: FunctionType? = null,
+    val isLoading: Boolean = false
+)
