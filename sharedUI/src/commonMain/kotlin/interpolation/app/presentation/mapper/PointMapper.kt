48cee54f64fdb71f1d79cda7f2cf0152cae9dcc2
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-06 16:29:39 +0300

    feat: added point mapper

---
+++ sharedUI/src/commonMain/kotlin/interpolation/app/presentation/mapper/PointMapper.kt
@@ -0,0 +1,22 @@
+package interpolation.app.presentation.mapper
+
+import interpolation.app.domain.model.Point
+import interpolation.app.presentation.basic.Mapper
+import interpolation.app.presentation.model.PointLabel
+import interpolation.app.presentation.parser.StringParser
+
+object PointMapper : Mapper<Point, PointLabel> {
+    override fun mapTo(t: Point): PointLabel {
+        return PointLabel(
+            StringParser.prepareToString(t.x),
+            StringParser.prepareToString(t.y)
+        )
+    }
+
+    override fun mapFrom(r: PointLabel): Point {
+        return Point(
+            StringParser.parseBigDecimal(r.x),
+            StringParser.parseBigDecimal(r.y)
+        )
+    }
+}
