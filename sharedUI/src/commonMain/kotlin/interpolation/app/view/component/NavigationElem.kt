145476c25f88139f5b3694c9c04881c7a6b2a5da
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-09 22:14:08 +0300

    fix: removed unised imports from navelem

--- sharedUI/src/commonMain/kotlin/interpolation/app/view/component/NavigationElem.kt
+++ sharedUI/src/commonMain/kotlin/interpolation/app/view/component/NavigationElem.kt
@@ -1,7 +1,6 @@
 package interpolation.app.view.component
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -10,18 +9,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import interpolation.app.theme.LocalAppDimens
 
 @Composable
