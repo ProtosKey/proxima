7a5caf1a6f7dabea026bc06ecf51dc0e3e1df776
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-10 01:47:20 +0300

    refactor: switch style changed

--- sharedUI/src/commonMain/kotlin/interpolation/app/view/feature/settings/component/Parameter.kt
+++ sharedUI/src/commonMain/kotlin/interpolation/app/view/feature/settings/component/Parameter.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import interpolation.app.theme.LocalAppDimens
 
 @Composable
 fun Parameter(
@@ -38,7 +42,17 @@ fun Parameter(
         }
         Switch(
             checked = isChecked,
-            onCheckedChange = switch
+            onCheckedChange = switch,
+            modifier = Modifier.scale(0.8f),
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = MaterialTheme.colorScheme.primary,
+                checkedTrackColor = MaterialTheme.colorScheme.primaryContainer,
+                checkedBorderColor = MaterialTheme.colorScheme.primary,
+
+                uncheckedThumbColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                uncheckedTrackColor = MaterialTheme.colorScheme.background,
+                uncheckedBorderColor = MaterialTheme.colorScheme.onSurfaceVariant
+            )
         )
     }
 }
