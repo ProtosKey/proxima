25d712e3a5b76de57fdb5d610cfbd1799721db75
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-10 17:39:12 +0300

    feat: added placeholder for input, added place for delete icon

--- sharedUI/src/commonMain/kotlin/interpolation/app/view/feature/input/component/Point.kt
+++ sharedUI/src/commonMain/kotlin/interpolation/app/view/feature/input/component/Point.kt
@@ -1,16 +1,21 @@
 package interpolation.app.view.feature.input.component
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,45 +31,72 @@ fun Point(
     onDelete: (Int) -> Unit
 ) {
     Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Max),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(
             LocalAppDimens.current.paddingSmall
         ),
-        modifier = Modifier.fillMaxWidth()
     ) {
+        /*
         Text(
             modifier = Modifier.size(LocalAppDimens.current.paddingLarge),
             text = "${index + 1}.",
             style = MaterialTheme.typography.bodyLarge,
         )
-
+         */
         OutlinedTextField(
             value = point.x,
+            shape = RoundedCornerShape(LocalAppDimens.current.radiusMedium),
             onValueChange = { onUpdate(index, it, point.y) },
-            label = { Text("X") },
+            placeholder = {
+                PlaceHolder("X", index)
+            },
             modifier = Modifier.weight(1f),
-            singleLine = true
+            singleLine = true,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f)
+            )
+
         )
 
         OutlinedTextField(
             value = point.y,
+            shape = RoundedCornerShape(LocalAppDimens.current.radiusMedium),
             onValueChange = { onUpdate(index, point.x, it) },
-            label = { Text("Y") },
+            placeholder = {
+                PlaceHolder("Y", index)
+            },
             modifier = Modifier.weight(1f),
-            singleLine = true
+            singleLine = true,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f)
+            )
         )
 
-        IconButton(
+        FilledIconButton(
+            modifier = Modifier
+                .fillMaxHeight()
+                .aspectRatio(1f),
+            shape = RoundedCornerShape(LocalAppDimens.current.radiusMedium),
             onClick = { onDelete(index) },
-            enabled = canDelete
+            enabled = canDelete,
+            colors =
+                IconButtonDefaults.filledIconButtonColors(
+                    containerColor = MaterialTheme.colorScheme.error,
+                    contentColor = MaterialTheme.colorScheme.onError,
+                    disabledContainerColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f),
+                    disabledContentColor = MaterialTheme.colorScheme.outline.copy(alpha = 0.5f)
+                )
         ) {
             Icon(
                 imageVector = Icons.Default.Delete,
                 contentDescription = "Удалить",
                 tint = if (canDelete)
-                    MaterialTheme.colorScheme.error
+                    MaterialTheme.colorScheme.onError
                 else
-                    MaterialTheme.colorScheme.outline
+                    MaterialTheme.colorScheme.background
             )
         }
     }
