1583d0e3deae2064ffb55d46b012d85bc88c3cf5
Rinat <rinat.gazizullin.31@bk.ru>
2026-05-10 05:03:20 +0300

    fix: added fixed main screen without graph

---
+++ sharedUI/src/commonMain/kotlin/interpolation/app/view/feature/graph/GraphScreen.kt
@@ -0,0 +1,33 @@
+package interpolation.app.view.feature.graph
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import interpolation.app.theme.LocalAppDimens
+import interpolation.app.view.component.NavigationBar
+import interpolation.app.view.component.Title
+
+class GraphScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Scaffold(
+            bottomBar = { NavigationBar(navigator) }
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(LocalAppDimens.current.paddingMedium)
+            ) {
+                Title(label = "Главная")
+            }
+        }
+    }
+}
